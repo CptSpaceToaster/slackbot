50c75acb40eb9147efc3803abc2480b199696095
CptSpaceToaster <CptSpaceToaster@gmail.com>
2015-05-02 13:46:09 -0400

    Cleaned up some schema errors/warnings
    Scheme was complaining when it attempted to parse the JSON into the
    payload struct.  This commit tidies adds fields to match the structure
    given my the outgoing webhook documentation
    
    Example:
    token=GdZEaKnlrbb2Uh3jwmi79lWT
    team_id=T0001
    team_domain=example
    channel_id=C2147483705
    channel_name=test
    timestamp=1355517523.000005
    user_id=U2147483697
    user_name=Steve
    text=googlebot: What is the air-speed velocity of an unladen swallow?
    trigger_word=googlebot:

--- robots/definitions.go
+++ robots/definitions.go
@@ -6,13 +6,17 @@ type SlashCommand struct {
 }
 
 type Payload struct {
-	Token       string `schema:"token"`
-	TeamID      string `schema:"team_id"`
-	ChannelID   string `schema:"channel_id"`
-	ChannelName string `schema:"channel_name"`
-	UserID      string `schema:"user_id"`
-	UserName    string `schema:"user_name"`
-	Text        string `schema:"text,omitempty"`
+	Token       string  `schema:"token"`
+	TeamID      string  `schema:"team_id"`
+	TeamDomain  string  `schema:"team_domain,omitempty"`
+	ChannelID   string  `schema:"channel_id"`
+	ChannelName string  `schema:"channel_name"`
+	Timestamp   float64 `schema:"timestamp,omitempty"`
+	UserID      string  `schema:"user_id"`
+	UserName    string  `schema:"user_name"`
+	Text        string  `schema:"text,omitempty"`
+	TriggerWord string  `schema:"trigger_word,omitempty"`
+	Service_ID  string  `schema:"service_id,omitempty"`
 	Robot       string
 }
 
