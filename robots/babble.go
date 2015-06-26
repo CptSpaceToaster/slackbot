2794616adccd5b5894934807c9072fa506480691
CptSpaceToaster <CptSpaceToaster@gmail.com>
2015-06-26 14:20:48 -0400

    Update babble-bot with a sentence parser
    It's still not amazing, but babble-bot now has some ability to parse
    sentences as it's writing them.  If it opens a quote, it will remember
    to close it later.  This is still a giant collection of hacks, but it's
    becoming a little more usable

--- robots/babble.go
+++ robots/babble.go
@@ -3,9 +3,13 @@ package robots
 import (
 	"bufio"
 	"bytes"
+	"encoding/json"
+	"flag"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 	"time"
@@ -14,26 +18,54 @@ import (
 type BabbleBot struct {
 }
 
-var dict = make(map[string][]string)
+type BabbleConfig struct {
+	Source string `json:"source"`
+}
+
+// Don't worry about this slice of Dictionaries that are keyed with strings, and reference slices of strings
+var Dict = make(map[string][]string)
+
+// Local Bot config
+var BotConfig = new(BabbleConfig)
+
+//var Dicts = make([]map[string][]string)
+
+// The regexp we use
+var WordRegex = regexp.MustCompile("([\\w’';,]+)|([\\.?!\"])")
+var PuncRegex = regexp.MustCompile("[\\.,?!]")
+var CommaRegex = regexp.MustCompile(",")
 
 func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	order := 0
+	flag.Parse()
+	configFile := filepath.Join(*ConfigDirectory, "babble.json")
+
+	if _, err := os.Stat(configFile); err == nil {
+		config, err := ioutil.ReadFile(configFile)
+		if err != nil {
+			log.Printf("ERROR: Error opening babblebot's config: %s", err)
+			return
+		}
+		err = json.Unmarshal(config, BotConfig)
+		if err != nil {
+			log.Printf("ERROR: Error parsing babblebot's config: %s", err)
+			return
+		}
+	} else {
+		log.Printf("WARNING: Could not find configuration file babble.json in %s", *ConfigDirectory)
+	}
+
+	index := 0
 	context := [2]string{}
 
 	w := &BabbleBot{}
 	RegisterRobot("babble", w)
 
-	regex, err := regexp.Compile("([\\w,’'\"-;]+)|([\\.?!])")
+	// TODO: Ability to read more than one file (avoid symlinks)
+	fh, err := os.Open(filepath.Join(*ConfigDirectory, BotConfig.Source))
 	if err != nil {
-		log.Println("Error in regex")
-		return // there was a problem with the regular expression.
-	}
-
-	fh, err := os.Open("dickens.txt")
-	if err != nil {
-		log.Println("It's not there")
+		log.Println("Bot source file not found")
 		return
 	}
 	defer fh.Close() // f.Close will run when we're finished.
@@ -48,18 +80,18 @@ func init() {
 			break
 		}
 
-		line := regex.FindAllString(string(buf), -1)
+		line := WordRegex.FindAllString(string(buf), -1)
 		for _, word := range line {
 			word_count++
 
-			context[order%len(context)] = word
-			order++
+			context[index%len(context)] = word
+			index++
 
 			var buffer bytes.Buffer
 
 			for i := 0; i < len(context)-1; i++ {
-				if context[(i+order)%len(context)] != "" {
-					buffer.WriteString(context[(i+order)%len(context)])
+				if context[(i+index)%len(context)] != "" {
+					buffer.WriteString(context[(i+index)%len(context)])
 					if i+2 != len(context) {
 						buffer.WriteByte(' ')
 					}
@@ -68,15 +100,15 @@ func init() {
 			//if word_count < 20 {
 			//	log.Println("<" + buffer.String() + ">[" + word + "]")
 			//}
-			dict[buffer.String()] = append(dict[buffer.String()], word)
+			Dict[buffer.String()] = append(Dict[buffer.String()], word)
 		}
 	}
-	log.Printf("Registerd %d words from dickens.txt", word_count)
+	log.Printf("Registerd %d words from %s", word_count, BotConfig.Source)
 
 	//s := regexp.MustCompile("\\s+").Split("This is a sentence", -1)
 	//log.Println(scanner.Text())
 
-	//split the text held by scanner by with regex by spaces
+	//split the text held by scanner by with WordRegex by spaces
 	//s := regexp.MustCompile("[^\\s]+").Split(scanner.Text())
 	//log.Println(scanner.Text())
 
@@ -88,7 +120,8 @@ func (w BabbleBot) Run(p *Payload) (slashCommandImmediateReturn string) {
 }
 
 func (w BabbleBot) DeferredAction(p *Payload) {
-	order := 0
+	//order := 0
+	index := 0
 	context := [2]string{}
 
 	var ret bytes.Buffer
@@ -117,34 +150,99 @@ func (w BabbleBot) DeferredAction(p *Payload) {
 		}
 		response.Send()
 	*/
-	order = len(context) - 1
+	index = len(context) - 1
 	if text == "" {
-		//TODO: arg parsing, and check to see if the thing is in the dictionary
+		//TODO: arg parsing, and check to see if the thing is in the Dictionary
 	}
 	context[0] = "The"
 	//context[1] = "lion"
 	context[1] = ""
 	ret.WriteString(context[0])
 
+	// TODO: These have to be set if the user types in a quote
+	// Keeps track of whether or not we have an open quote.  Nested quotes are not possible atm
+	quote := false
+	quote_flag := false
+	punc_flag := false
+	last_word := ""
+
 	for {
 		var buffer bytes.Buffer
 		for i := 0; i < len(context)-1; i++ {
-			if context[(i+order+1)%len(context)] != "" {
-				buffer.WriteString(context[(i+order+1)%len(context)])
+			if context[(i+index+1)%len(context)] != "" {
+				buffer.WriteString(context[(i+index+1)%len(context)])
 				if i+2 != len(context) {
 					buffer.WriteByte(' ')
 				}
 			}
 		}
 
-		s := dict[buffer.String()][rand.Intn(len(dict[buffer.String()]))]
-		context[order%len(context)] = s
-		ret.WriteString(" " + s)
-		if s[len(s)-1] == '.' {
-			break
+		// Obtain word
+		s := Dict[buffer.String()][rand.Intn(len(Dict[buffer.String()]))]
+
+		// Immediatly exit if we chose another puncuation mark when the punctuation flag was set
+		if PuncRegex.MatchString(s) && punc_flag == true {
+			continue
+		} else {
+			punc_flag = true
 		}
 
-		order++
+		//TODO: Make this a case statement, and handle quotes
+		//TODO: Quotes can follow commas, and they look weird if they close
+		switch s {
+		case "\"":
+			// If we JUST started a quote
+			if quote_flag {
+				continue
+			}
+
+			if quote {
+				// If the last word was a comma, we actually DON'T want to put a quote there
+				if CommaRegex.MatchString(last_word) {
+					continue
+				} else {
+					ret.WriteString(s)
+				}
+			} else {
+				// If we open a quote, set the quote flag
+				ret.WriteString(" " + s)
+				quote_flag = true
+			}
+			// Toggle whether we are in a quote or not. TODO: Quotes can't be nested
+			quote = !quote
+			// Set the puncuation flag
+			punc_flag = true
+		case ".":
+			ret.WriteString(s)
+		case "?":
+			ret.WriteString(s)
+		case "!":
+			ret.WriteString(s)
+		default:
+			if quote_flag {
+				// If the quote_flag is set, then don't print a leading space, and consume the flag
+				ret.WriteString(s)
+				quote_flag = false
+			} else {
+				ret.WriteString(" " + s)
+			}
+			// Consume the puncuation flag
+			punc_flag = false
+		}
+
+		// Accept the new word
+		context[index%len(context)] = s
+
+		// End sequence
+		if s == "." {
+			if quote {
+				// Close a dangeling quote
+				ret.WriteString("\"")
+			}
+			break
+		}
+		last_word = s
+		index++
 	}
 	response.Text = ret.String()
 	response.Send()
